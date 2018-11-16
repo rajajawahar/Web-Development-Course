aec3f5690a320930b10501841c1adafe82fc791f
Raja Jawahar <rajajawahar77@gmail.com>
2018-11-16 22:54:29 +0530

    Python basics

---
+++ python/hello-world.py
@@ -0,0 +1,19 @@
+print "hello workld"
+
+age = 25
+
+name = "Raja Mohamed"
+list = {"First Name","Last Name"}
+tuples = (1,2,3,4)
+dictionary ={}
+dictionary["FirstName"] = "Raja"
+dictionary["LastName"] = "Mohamed"
+dictionary["MobileNo"] = "9994267918"
+
+print list
+print tuples
+print dictionary
+
+print age
+print name
+
