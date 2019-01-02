b31452a3b82dbc6d6c1d48e4f2b7c60619b70f57
Raja Jawahar <rajajawahar77@gmail.com>
2019-01-02 16:39:12 +0530

    Conditional Operators added

--- php/index.php
+++ php/index.php
@@ -1,6 +1,36 @@
 <?php
 
+
+// Array Example
 $myarray = array("Raja","Moahmed","Jawahar","Ismail");
-print_r($myarray)
+// print_r($myarray);
+echo $myarray[0];
+
+// Conditional Statements
+
+$user = "User";
+echo "<br>";
+
+if($user =="User"){
+    print("User is present");
+}else{
+    print("There is no User");
+}
+echo "<br>";
+
+// ForLoop Statements
+
+for($i=0;$i<10;$i++){
+    echo $i."<br>";
+}
+
+for($i=0;$i<sizeof($myarray);$i++){
+    echo $myarray[$i]."<br>";
+}
+
+foreach($myarray as $key => $value){
+    echo $key."<br>";
+    echo $value."<br>";
+}
 
 ?>
