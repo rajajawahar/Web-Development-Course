c2bd945abc15a6a213007978323d42f89f631c14
Raja Jawahar <rajajawahar77@gmail.com>
2019-02-13 16:31:33 +0530

    Post Variables added

--- php/index.php
+++ php/index.php
@@ -1,36 +1,66 @@
 <?php
 
 
-// Array Example
-$myarray = array("Raja","Moahmed","Jawahar","Ismail");
-// print_r($myarray);
-echo $myarray[0];
+// // Array Example
+// $myarray = array("Raja","Moahmed","Jawahar","Ismail");
+// // print_r($myarray);
+// echo $myarray[0];
 
-// Conditional Statements
+// // Conditional Statements
 
-$user = "User";
-echo "<br>";
+// $user = "User";
+// echo "<br>";
 
-if($user =="User"){
-    print("User is present");
-}else{
-    print("There is no User");
+// if($user =="User"){
+//     print("User is present");
+// }else{
+//     print("There is no User");
+// }
+// echo "<br>";
+
+// // ForLoop Statements
+
+// for($i=0;$i<10;$i++){
+//     echo $i."<br>";
+// }
+
+// for($i=0;$i<sizeof($myarray);$i++){
+//     echo $myarray[$i]."<br>";
+// }
+
+// foreach($myarray as $key => $value){
+//     echo $key."<br>";
+//     echo $value."<br>";
+// }
+
+
+
+
+if($_POST){
+    $myarray = array("Raja","Rahul","Ram","Rathna");
+
+    $isKnown = false;
+foreach($myarray as $value){
+    if($value == $_POST['name']){
+        $isKnown = true;
+    }
 }
-echo "<br>";
-
-// ForLoop Statements
-
-for($i=0;$i<10;$i++){
-    echo $i."<br>";
+if($isKnown){
+    echo " Hi there".$_POST['name'];
 }
-
-for($i=0;$i<sizeof($myarray);$i++){
-    echo $myarray[$i]."<br>";
+else{
+    echo "I dont know you";
 }
-
-foreach($myarray as $key => $value){
-    echo $key."<br>";
-    echo $value."<br>";
 }
-
 ?>
+
+<form method="post">
+
+    <p>What is your name?</p>
+    
+    <p><input type="text" name="name"></p>
+    
+    <p><input type="submit" value="Submit"></p>
+
+
+</form>
